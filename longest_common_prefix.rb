7873e594605b82f883e571a77c36d46603444916
Meredith Trevino <meredithtrevino3@gmail.com>
2023-09-26 16:43:28 -0600

    adds solution for longest_common_prefix

--- longest_common_prefix.rb
+++ longest_common_prefix.rb
@@ -21,6 +21,19 @@
 # 0 <= strs[i].length <= 200
 # strs[i] consists of only lowercase English letters.
 
+#things to consider: 
+#input: array of strings
+#output: string of the longest common prefix
+#edge cases: no common prefix, empty array, empty string
 def longest_common_prefix(array_of_strings)
-  
+  return "" if array_of_strings.empty?
+  return array_of_strings[0] if array_of_strings.length == 1
+  prefix = ""
+  array_of_strings[0].each_char.with_index do |char, index|
+    array_of_strings.each do |string|
+      return prefix if string[index] != char
+    end
+    prefix += char
+  end
+  prefix
 end
