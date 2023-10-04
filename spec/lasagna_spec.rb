e1b72e6251eabd87318071eea2b4cb289d68d6b4
Meredith Trevino <meredithtrevino3@gmail.com>
2023-10-04 16:26:12 -0600

    adds expected minutes in oven test

--- spec/lasagna_spec.rb
+++ spec/lasagna_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Lasagna do
     it 'exists' do
       expect(lasagna).to be_a(Lasagna)
     end
+
+    it 'returns 40 for expected time in oven' do
+      expect(Lasagna::EXPECTED_MINUTES_IN_OVEN).to eq(40)
+    end
   end
 end
