6f2d03736c384e43b09f0ff5b6d1d1d12e4595ae
Meredith Trevino <meredithtrevino3@gmail.com>
2023-10-09 12:14:10 -0600

    beginning solution for calculator problem

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,30 @@
 class Calculator
+    ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
   
+    def self.calculate(first_operand, second_operand, operation)
+  
+      if first_operand.is_a?(String) || second_operand.is_a?(String)
+        raise ArgumentError
+      end
+    
+      string_answer = []
+      string_answer << [first_operand, operation, second_operand]
+      
+      if operation == '+'
+        answer = first_operand + second_operand
+        string_answer << ['=', answer]
+        string_answer.flatten.join(' ')
+      elsif operation == '/'
+        return 'Division by zero is not allowed.' if second_operand == 0
+        answer = first_operand/second_operand
+        string_answer << ['=', answer]
+        string_answer.flatten.join(' ')
+      elsif operation == '*'
+        answer = first_operand * second_operand
+        string_answer << ['=', answer]
+        string_answer.flatten.join(' ')
+      else operation == nil || operation == ' ' || operation == '**'
+        raise UnsupportedOperation
+      end
+    end  
 end
