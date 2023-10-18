1798a9b41c27b8306fc45232b88c1df300ca8549
Meredith Trevino <meredithtrevino3@gmail.com>
2023-10-18 14:11:57 -0600

    adds three tests for contains duplicates

--- contains_duplicates.rb
+++ contains_duplicates.rb
@@ -25,3 +25,7 @@
 def contains_duplicates(numbers)
   numbers.size != numbers.uniq.size
 end
+
+puts contains_duplicates([1,2,3,1])
+puts contains_duplicates([1,2,3,4])
+puts contains_duplicates([1,1,1,3,3,4,3,2,4,2])
